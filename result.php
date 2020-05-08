e56b01669c66f9273acf95d23d0a93b026269b63
EleanorGH <48810496+EleanorGH@users.noreply.github.com>
2020-05-08 13:33:21 +1000

    Add files via upload

---
+++ result.php
@@ -0,0 +1,116 @@
+<html>
+
+<head>
+	<?php
+	$name = $_GET['name'] ;
+	$age = $_GET['age'] ;
+	$color = $_GET["color"] ;
+	$dog = $_GET["dog"]
+
+	?>
+	<style type="text/css">
+
+		@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+		body {
+			background-color: <?php echo $color ; ?> ;
+			font-family: "Open Sans" ;
+		}
+
+		h1 {
+			margin-top: 20px;
+			color: white;
+			font-weight: normal;
+			font-style: italic;
+			font-size: 46px;
+			text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+		}
+
+		a {
+			color: black;
+			text-decoration: underline;
+		}
+		
+		a img {
+			display: none;
+		}
+
+		a:hover img {
+			display: block;
+			width: 60%;
+			border: 12px inset white;
+		}
+
+		.disclaimer {
+			font-size: 12px;
+		}
+
+		
+	</style>
+</head>
+
+<body>
+
+<?php
+
+echo '<h1>Welcome to the baguette, ' . $age . ' year old ' . $name .'.</h1>' ;
+echo '<p>Here you won\'t find much. I\'m terribly sorry to disappoint, but my task outline 
+didn\'t tell me to do anything else. If you\'re really upset though, you can mouse over this' ;
+
+//-----------------DOG IMAGES---------------------------
+
+if ($dog == "dalmation") {
+	print '<a href="#"> word <img src=
+	"https://www.storytrender.com/wp-content/uploads/2018/04/9_CATERS_DALMATIAN_HEART_NOSE_10-1024x683.jpg"/></a>' ;
+}
+
+if ($dog == 'golden') {
+	print '<a href="#"> word <img src=
+	"https://static.boredpanda.com/blog/wp-content/uploads/2017/07/cute-golden-retriever-puppies-202-5967223e5ba31__605.jpg"/></a>' ;
+}
+
+if ($dog == 'pug') {
+	print '<a href="#"> word <img src=
+	"https://images-na.ssl-images-amazon.com/images/I/714unRWIbVL._AC_SL1500_.jpg"/></a>' ;
+}
+
+if ($dog == 'husky') {
+	print '<a href="#"> word <img src=
+	"https://lh3.googleusercontent.com/proxy/_Qk5tNwvaoeY3GnXRIHbCgbtE6vdcTcI1xlZX7ZgDFslUODfixVbc5ap-DZP_6e8xS71GQV4AuFmNGYgFWYt3a4D_2Bji6qGUjoahJn27Jcy6oDu4sHTVA"/></a>' ;
+}
+
+if ($dog =='bernese') {
+	print '<a href="#"> word <img src=
+	"https://i.pinimg.com/originals/10/4c/dd/104cdd7ec7276f02239b7171ea4fae5a.jpg"/></a>' ;
+}
+
+if ($dog == 'poodle') {
+	print '<a href="#"> word <img src=
+	"https://i.ytimg.com/vi/fNYJvUs_OiA/maxresdefault.jpg"/></a>' ;
+}
+
+if ($dog === 'bulldog') {
+	print '<a href="#"> word <img src=
+	"https://www.pets4homes.co.uk/images/articles/4352/large/how-much-should-you-pay-for-an-english-bulldog-puppy-59ae99983c911.jpg"/></a>' ;
+}
+
+if ($dog == 'yorkshire') {
+	print '<a href="#"> word <img src=
+	"https://cdn.wallpapersafari.com/56/47/qCoUxk.jpg"/></a>' ;
+}
+
+if ($dog == 'corgi') {
+	print '<a href="#"> word <img src=
+	"https://wallpapercave.com/wp/wp4691321.jpg"/></a>' ;
+}
+
+echo "for a special surprise!</p>" ;
+echo "<p class='disclaimer'><b>DISCLAIMER:</b> I do not own the right to any images displayed on the site - links to the original images
+		can be found in the code for this website.</p>" ;
+
+?>
+
+</body>
+
+</html>
